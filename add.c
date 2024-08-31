655a3ebbee7f63e06e2ea8d4f6952555a657583f
Abdoaelnabalawy <abdullahelnabalawy@gmail.com>
2024-08-31 10:21:11 -0700

    Create add.c

---
+++ add.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int add (int x , int y )
+{
+    return x+y ; 
+}
+int main() {
+    // Write C code here
+    // printf("hello world ! ");
+    int a = 10 ; int b = 5 ;
+    int c = add (a,b) ;
+    printf(" c = %d",c);
+    return 0;
+}
